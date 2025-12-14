7c076de64ff92227cfdfc0867376aa4534a25e7f
poco212 <baihaqia442@gmail.com>
2025-12-14 09:27:17 +0700

    update

--- post/install.sh
+++ post/install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 #hostname
-echo "testing" > /etc/hostname
+echo "testing" > /etc/hostname &&
+clear &&
+echo "hostname done"
+sleep 2
+
+#timectl
+clear &&
+ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime &&
+hwclock --systohc &&
+timedatectl set-ntp true &&
+timedatectl set-timezone Asia/Jakarta &&
+timedatectl status &&
+timedatectl show-timesync --all &&
+clear &&
+echo "hostname done"
+sleep 2
+
+#locale
+clear &&
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen &&
+locale-gen &&
 clear &&
 echo "hostname done"
 sleep 2
